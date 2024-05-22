9196b9eba2f930a3b49638fed7ad4dcbd00fd105
diegotorresg93 <diegotorresg93@gmail.com>
2024-05-21 21:02:14 -0500

    Operadores de comparacion

--- operadoresdecomparacion.php
+++ operadoresdecomparacion.php
@@ -2,13 +2,25 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Documento sin título</title>
-</head>
-<?php
+<set language="es">
+<title>Operadores de comparación</title>
+<body>
+	<?php
 	
+	$edad =16;
+	if($edad>40){
+		
+		print "<p>Bienvenido a nuestra pagina, esperemos no sea peligroso para su salud...</p>";
+		} else if($edad>30){
+		print "<p>Sabemos que aqui encontrara lo que lleva mucho tiempo buscando...</p>";
+	    }else if ($edad>18){
+		print "<p>Estas en tu momento de comprar nuestros productos</p>";
+	    }else {
+		print "<p>Lo sentimos, no puedes tener acceso a la pagina</p>";
+	    }
+	print "<p>Tu edad es de $edad años</p>";
 	
 	
 	?>
-<body>
 </body>
 </html>
