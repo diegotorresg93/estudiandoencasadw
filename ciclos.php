49408a5c10c673cdb3dc6ed63776d01177161ee4
Diego Torres <diegotorresg93@gmail.com>
2024-06-18 21:13:06 -0500

    Ciclos

--- ciclos.php
+++ ciclos.php
@@ -6,6 +6,18 @@
     <title>Ciclos</title>
 </head>
 <body>
+    <?php
+    //while
+    $contador = 0;
+    while ($contador<10){
+
+        print "<p>El valor de contador es $contador</p>";
+        $contador++;
+    }
+    
+    print "<p>Fin del ciclo, el valor del contador es $contador</p>";
+    
+    ?>
     
 </body>
 </html>
